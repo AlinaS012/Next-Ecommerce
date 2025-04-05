4d5be9fffad61e48eb5dd1dc22ad6bc29d9dac11
Alina Soomro <alina.soomro@yahoo.com>
2025-04-05 17:40:38 +0500

    update for build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import CategoryList from "@/components/CategoryList";
 import ProductList from "@/components/ProductList";
 import Skeleton from "@/components/Skeleton";
 import Slider from "@/components/Slider";
-import UseSupabase from "@/components/UseSupabase";
+// import UseSupabase from "@/components/UseSupabase";
 // import { SupabaseClient } from "@supabase/supabase-js";
 // import { redirect, useRouter } from "next/navigation";
 import { Suspense } from "react";
@@ -12,7 +12,7 @@ const HomePage = () => {
   return (
     <div className="">
       {/* <CheckStatus/> */}
-      <UseSupabase />
+      {/* <UseSupabase /> */}
       <Slider />
       <div className="mt-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
         <h1 className="text-2xl">Featured Products</h1>
